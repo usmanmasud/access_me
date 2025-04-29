15c3fe8d7f5cb1b863c20895aa8283449ea8f6ef
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-29 22:55:08 +0100

    update clientsts

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAunNYMbIU_4MVN_y2lxiCRqOFyM0YQzhI",
