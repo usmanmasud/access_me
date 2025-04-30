8145c8d874777e4f89079844ec43fab2c367204a
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-30 17:56:56 +0100

    working

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -82,3 +82,37 @@ export async function signIn(params: SignInParams) {
     };
   }
 }
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log("error in finding user", error);
+
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+
+  return !!user;
+}
