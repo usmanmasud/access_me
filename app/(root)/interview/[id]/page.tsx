4acb4e9bda05949ccc3214e4190e03692d63e4c7
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-02 06:55:53 +0100

    individual caed route set

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { getInterviewById } from "@/lib/actions/general.action";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async ({ params }: RouteParams) => {
+  const { id } = await params;
+
+  const interview = await getInterviewById(id);
+
+  if (!interview) redirect("/");
+
   return <div>page</div>;
 };
 
