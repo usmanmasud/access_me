63dd79bad36b9c53f90c9286b1851f9a85823c63
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-02 06:39:19 +0100

    refactor general auht

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -116,36 +116,3 @@ export async function isAuthenticated() {
 
   return !!user;
 }
-
-export async function getLatestInterviews(
-  params: GetLatestInterviewsParams
-): Promise<Interview[] | null> {
-  const { userId, limit = 20 } = params;
-
-  const interviews = await db
-    .collection("interviews")
-    .orderBy("createdAt", "desc")
-    .where("finalized", "==", true)
-    .where("userId", "!=", userId)
-    .limit(limit)
-    .get();
-
-  return interviews.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Interview[];
-}
-export async function getInterviewByUserId(
-  userid: string
-): Promise<Interview[] | null> {
-  const interviews = await db
-    .collection("interviews")
-    .where("userId", "==", userid)
-    .orderBy("createdAt", "desc")
-    .get();
-
-  return interviews.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Interview[];
-}
